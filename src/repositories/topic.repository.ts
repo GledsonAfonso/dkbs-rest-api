90a0557bf8c6d7747530a9cccd56dc82a6d02be8
Gledson A <g_a6@live.com>
2025-04-30 12:42:52 -0300

    Adding Topic repository

---
+++ src/repositories/topic.repository.ts
@@ -0,0 +1,81 @@
+import { getDatabaseClient } from "@config/database";
+import { NotFoundError } from "@models/errors";
+import { Topic } from "@models/topic";
+
+const databaseClient = getDatabaseClient();
+
+type IdParam = {
+  id: number;
+};
+
+type IdAndVersionParams = IdParam & {
+  version: number;
+};
+
+type UpdateTopicParams = Partial<Topic> & IdAndVersionParams;
+
+const createTopic = async (data: Topic): Promise<Topic> => {
+  return databaseClient.topic.create({
+    data,
+  });
+};
+
+const findTopicByIdAndVersion = async (data: IdAndVersionParams): Promise<Topic | null> => {
+  return databaseClient.topic.findUnique({
+    where: {
+      topicId: {
+        id: data.id,
+        version: data.version,
+      }
+    },
+  });
+};
+
+const findLatestTopicWithChildTopics = async (data: IdParam): Promise<Topic | null> => {
+  return databaseClient.topic.findFirst({
+    where: {
+      id: data.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    }
+  });
+};
+
+const updateTopic = async (data: UpdateTopicParams): Promise<Topic> => {
+  const previousTopic = await findTopicByIdAndVersion({
+    id: data.id,
+    version: data.version,
+  });
+
+  if (!previousTopic) {
+    throw new NotFoundError(`Topic with id ${data.id} and version ${data.version} was not found.`);
+  }
+
+  const newTopic: Topic = {
+    ...previousTopic,
+    version: previousTopic.version + 1, // iterating the field for a new version
+
+    // overriding dates to facilitate future searches for latest versions
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  return createTopic(newTopic);
+};
+
+const deleteTopic = async (data: IdParam) => {
+  return databaseClient.topic.deleteMany({
+    where: {
+      id: data.id,
+    }
+  })
+};
+
+export {
+  createTopic,
+  findTopicByIdAndVersion,
+  findLatestTopicWithChildTopics,
+  updateTopic,
+  deleteTopic,
+};
